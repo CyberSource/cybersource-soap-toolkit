0055ac44fc3c24c4f8562cca203bc79209c7d7cc
gnongsie <gnongsie@visa.com>
2025-02-12 21:41:39 +0530

    Method extraction

--- JavaSoapToolkit/src/main/java/com/cybersource/BinarySecurityTokenHandler.java
+++ JavaSoapToolkit/src/main/java/com/cybersource/BinarySecurityTokenHandler.java
@@ -20,43 +20,48 @@ public class BinarySecurityTokenHandler implements SOAPHandler<SOAPMessageContex
         if (outboundProperty) {
             SOAPMessage soapMessage = soapMessageContext.getMessage();
 
-            try {
-                // (i) Fetch SOAP envelope
-                SOAPEnvelope soapEnvelope = soapMessage.getSOAPPart().getEnvelope();
-                SOAPBody soapBody = soapMessage.getSOAPBody();
-                soapBody.addAttribute(soapEnvelope.createName("Id", "wsu",
-                        "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"), "Body");
-                SOAPHeader soapHeader = soapEnvelope.getHeader();
-                if (soapHeader == null) {
-                    soapHeader = soapEnvelope.addHeader();
-                }
-
-                // (ii) Add security envelope
-                SOAPElement securityElement = soapHeader.addChildElement("Security", "wsse",
-                        "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
-                securityElement.addNamespaceDeclaration("wsu",
-                        "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
-
-                // (iii) Add Binary Security Token envelope
-                SOAPElement tokenElement = securityElement.addChildElement("BinarySecurityToken", "wsse");
-                tokenElement.setAttribute("ValueType",
-                        "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3");
-                tokenElement.setAttribute("EncodingType",
-                        "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary");
-                tokenElement.setAttribute("wsu:Id", "X509Token");
-                tokenElement.addTextNode(SecurityUtils.generateBinarySecurityToken());
-
-                // (iv) Combine Binary Security Token with Signature
-                SOAPElement securityTokenReferenceElement = securityElement.addChildElement("SecurityTokenReference", "wsse");
-                SOAPElement referenceElement = securityTokenReferenceElement.addChildElement("Reference", "wsse");
-                referenceElement.setAttribute("URI", "#X509Token");
-
-                SecurityUtils.createDetachedSignature(securityElement, SecurityUtils.getKeyFromCertificate(), securityTokenReferenceElement);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            Boolean isMessageSent = processSoapMessage(soapMessage);
         }
         return true;
+    }
+
+    public boolean processSoapMessage(SOAPMessage soapMessage) {
+        try {
+            // (i) Fetch SOAP envelope
+            SOAPEnvelope soapEnvelope = soapMessage.getSOAPPart().getEnvelope();
+            SOAPBody soapBody = soapMessage.getSOAPBody();
+            soapBody.addAttribute(soapEnvelope.createName("Id", "wsu",
+                    "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"), "Body");
+            SOAPHeader soapHeader = soapEnvelope.getHeader();
+            if (soapHeader == null) {
+                soapHeader = soapEnvelope.addHeader();
+            }
+
+            // (ii) Add security envelope
+            SOAPElement securityElement = soapHeader.addChildElement("Security", "wsse",
+                    "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+            securityElement.addNamespaceDeclaration("wsu",
+                    "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
+
+            // (iii) Add Binary Security Token envelope
+            SOAPElement tokenElement = securityElement.addChildElement("BinarySecurityToken", "wsse");
+            tokenElement.setAttribute("ValueType",
+                    "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3");
+            tokenElement.setAttribute("EncodingType",
+                    "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary");
+            tokenElement.setAttribute("wsu:Id", "X509Token");
+            tokenElement.addTextNode(SecurityUtils.generateBinarySecurityToken());
+
+            // (iv) Combine Binary Security Token with Signature
+            SOAPElement securityTokenReferenceElement = securityElement.addChildElement("SecurityTokenReference", "wsse");
+            SOAPElement referenceElement = securityTokenReferenceElement.addChildElement("Reference", "wsse");
+            referenceElement.setAttribute("URI", "#X509Token");
+
+            SecurityUtils.createDetachedSignature(securityElement, SecurityUtils.getKeyFromCertificate(), securityTokenReferenceElement);
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
